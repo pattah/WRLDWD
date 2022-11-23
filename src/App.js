4411a5547773b2582bc05a559c10820084d7ab92
pattah <rosiak211@gmail.com>
2022-11-23 13:10:50 +0100

    routes test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 import {HomeTab, ShopTab, ContactTab, TermsInfoTab, CartTab} from '../src/components/navTabs/NavTabs';
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Routes>
-        <Route exact path="/" element={<HomeTab />}></Route>
+        <Route path="/" element={<HomeTab />}></Route>
         <Route path="/Shop" element={<ShopTab />}></Route>
         <Route path="/Contact" element={<ContactTab />}></Route>
         <Route path="/TermsInfo" element={<TermsInfoTab />}></Route>
         <Route path="/Cart" element={<CartTab />}></Route>
       </Routes>
-    </BrowserRouter>
+    </>
   )
 }
 
