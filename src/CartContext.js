c1ac67a5c219eaa2ed9b89c61ee5f21675381726
pattah <rosiak211@gmail.com>
2022-11-29 20:27:27 +0100

    shopping cart test

--- src/CartContext.js
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-import { products } from '../../pages/Shop/ItemsData';
-
-const CartContext 
--- src/components/NavTabs.js
+++
@@ -1,7 +0,0 @@
-import HomeTab from '../pages/Home/HomeTab';
-import ShopTab from '../pages/Shop/ShopTab';
-import ContactTab from '../pages/Contact/ContactTab';
-import TermsInfoTab from '../pages/TermsAndInfo/TermsInfoTab';
-import CartTab from '../pages/Cart/CartTab';
-
-export {HomeTab, ShopTab, ContactTab, TermsInfoTab, CartTab}
