90a1af37ff76c15830aa1467966d3b91ee0fb225
pattah <rosiak211@gmail.com>
2022-11-30 01:07:34 +0100

    stripe api test

---
+++ ecommerce/server/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+var cors = require('cors');
+const stripe = require('stripe')('sk_test_51M9Yu3HuRfY9hVxVoAQKv2Ttxd6gWVZp9EL9vFUwTX0WSwfQn3upnudv8VBTS1VUqBsl6iTZ9WlXqsQMArbd3kNu00HXMdvLnJ');
+
+const app = express();
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.json());
+
+app.post("/checkout", async (req, res) => {
+ 
+  console.log(req.body);
+  const items = req.body.items;
+  let lineItems = [];
+  items.forEach((item)=> {
+    lineItems.push(
+      {
+        price: item.id,
+        quantity: item.quantity
+      }
+    )
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: lineItems,
+    mode: 'payment',
+    success_url: "http://localhost:3000/#/success",
+    cancel_url: "http://localhost:3000/cancel"
+  })   
+  res.send(JSON.stringify({
+    url: session.url
+  }));
+});
+
+app.listen(4000, () => console.log("Listening on port 4000!"));
